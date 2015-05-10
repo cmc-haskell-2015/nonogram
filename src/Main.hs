2e1c1fb64cbae852f35bbc833d2c51af630ae80d
Murat <mura020395@gmail.com>
2015-05-11 01:24:58 +0300

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,9 @@
 module Main where
 
 import Graphics.Gloss.Interface.Pure.Game
-
 import Control.Applicative
-
 import Logic
-
 import Solver
-
 import Draw
 
 main :: IO ()
@@ -26,203 +22,74 @@ startGame world
     	renderer              
     	handler
     	updater
-
-renderer Field  { board 	= board
-				, boardSize = (i, j)
-				, leftPanel = left
-				, topPanel  = top
-				, panelSize = (n, m)
-				, gameOver  = flag
-				} 
-	= translate xx yy (scale (scGlob (i, j)) (scGlob (i, j)) world)
+--функция для отрисовки всего FIELD
+renderer :: Field -> Picture
+renderer Field 	{ board 	  = board
+								, boardSize = (i, j)
+								, leftPanel = left
+								, topPanel  = top
+								, panelSize = (n, m)
+								, gameOver  = flag
+								}
+	= translate xx yy (scale worldScale worldScale world)
 	where
-		world		  = pictures [boardpic, leftTopRect, miniPic, leftPic, topPic, gameOverPic]
+		world = pictures [boardpic, leftTopRect, miniPic, leftPic, topPic, gameOverPic]
 		boardpic      = drawBoard board (i, j)
 		leftTopRect   = translate (x / 2) (y / 2) (rectangleWire (-x) y)
 		miniPic       = translate x y miniBoard
 		leftPic       = translate x 0 left'
 		topPic        = translate 0 y top'
 		gameOverPic   = (drawGameOverPic flag (fj * 5) (-fi * 5))
-		left'  		  = drawLeftNumPanel left (i, n)
-		top'   		  = drawTopNumPanel top (m, j)
+		left'  		 		= drawLeftNumPanel left (i, n)
+		top'   		  	= drawTopNumPanel top (m, j)
 		miniBoard     = scale sx sy (drawBoard (oBoard board) (i, j))
 		[fn,fj,fm,fi] = intToFloat [n,j,m,i]
-		sx = fn / fj
-		sy = fm / fi
-		x  = fn * (-10.0)
-		y  = fm * 10.0
-		xx = fj * (-5.0 * scGlob (i, j))
-		yy = fi * (5.0 * scGlob (i, j))
-
-scGlob :: (Int, Int) -> Float
-scGlob (i, j) = 50.0 / mm
-	where
-		[mm] = intToFloat [max i j] 
-
-		
-
---ЛЕВАЯ КНОПКА МЫШИ
-handler (EventKey (MouseButton LeftButton) Down _ (x, y)) 
-	Field 	{ board       = board
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = gameOver
-			}
-	| and [x > -sizeX, x < sizeX, y > -sizeY, y < sizeY] 
-		= Field { board 	  = putCellToBoard (ii, jj) board O
-				, boardSize   = (i, j)
-				, leftPanel   = left
-				, topPanel    = top
-				, leftAutomat = lAuto
-				, topAutomat  = tAuto
-				, panelSize   = panelSize
-				, gameOver    = gameOver
-				}
+		worldScale	  = scGlob (i, j)
+		sx 						= fn / fj
+		sy 						= fm / fi
+		x  						= fn * (-10.0)
+		y  						= fm * 10.0
+		xx 						= fj * (-5.0) * worldScale
+		yy 						= fi * 5.0 * worldScale
+-- обрабатываются действия пользователя, в зависимости от них изменяется FIELD
+handler :: Event -> Field -> Field
+--обработка нажатия кнопок мыши
+handler (EventKey (MouseButton mbutton) Down _ (x, y)) 
+	f@Field	{ board     = board
+					, boardSize = (i, j)
+					}
+	| and [x > -sizeX, x < sizeX, y > -sizeY, y < sizeY, mbutton == LeftButton] 
+		= f	{ board = putCellToBoard (ii, jj) board O } 
+	| and [x > -sizeX, x < sizeX, y > -sizeY, y < sizeY, mbutton == RightButton]
+		= f { board = putCellToBoard (ii, jj) board X }
 		where
 			[fj, fi] = intToFloat [j, i]
 			sizeX = fj * scGlob (i, j) * 5.0
 			sizeY = fi * scGlob (i, j) * 5.0 
 			ii = - truncate ((y - sizeY) / (10.0 * scGlob (i, j)))
 			jj = truncate ((x + sizeX) / (10.0 * scGlob (i, j)))
-
---ПРАВАЯ КНОПКА МЫШИ
-handler (EventKey (MouseButton RightButton) Down _ (x, y)) 
-	Field 	{ board       = board
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = gameOver
-			}
-	| and [x > -sizeX, x < sizeX, y > -sizeY, y < sizeY] 
-		= Field { board 	  = putCellToBoard (ii, jj) board X
-				, boardSize   = (i, j)
-				, leftPanel   = left
-				, topPanel    = top
-				, leftAutomat = lAuto
-				, topAutomat  = tAuto
-				, panelSize   = panelSize
-				, gameOver    = gameOver
-				}
-		where
-			[fj, fi] = intToFloat [j, i]
-			sizeX = fj * (scGlob (i, j)) * 5.0
-			sizeY = fi * (scGlob (i, j)) * 5.0 
-			ii = - truncate ((y - sizeY) / (10.0 * scGlob (i, j)))
-			jj = truncate ((x + sizeX) / (10.0 * scGlob (i, j)))
-
-handler (EventKey (Char 'i') Down _ _) 
-	Field 	{ board       = board
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = False
-			}
-	= Field { board       = nextStepBoard
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = newFlag
-			}
+--обработка нажатия клавиш
+handler (EventKey (Char key) Down _ _)
+	f@Field	{ board       = board
+					, boardSize 	= (i, j)
+					, leftPanel   = left
+					, topPanel 	  = top
+					, solverAuto  = Auto { leftAutomat = lAuto
+															 , topAutomat  = tAuto }
+					}
+	| key == 'r' = f { board = emptyBoard i j }
+	|	key == 'i' = f { board = nextStepBoardRows }
+	|	key == 'j' = f { board = nextStepBoardColumns }
+	| key == 'g' = f { board = finishBoard }
 	where
-		newFlag = checkToWin nextStepBoard lAuto
-		nextStepBoard = autoSolution board left lAuto
-
-handler (EventKey (Char 'j') Down _ _) 
-	Field 	{ board       = board
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = False
-			}
-	= Field { board       = nextStepBoard
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = newFlag
-			}
-	where
-		newFlag = checkToWin nextStepBoard lAuto
-		nextStepBoard = transpose (autoSolution (transpose board) top tAuto)
-
-handler (EventKey (Char 'g') Down _ _) 
-	Field 	{ board       = board
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = False
-			}
-	= Field { board       = finishBoard
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = newFlag
-			}
-	where
-		newFlag = checkToWin finishBoard lAuto
-		finishBoard = autoGen (emptyBoard i j) left top lAuto tAuto 0
-
-handler (EventKey (Char 'r') Down _ _) 
-	Field 	{ board       = board
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = flag
-			}
-	= Field { board       = emptyBoard i j
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = False
-			}
-			
+		nextStepBoardRows    = autoSolution board left lAuto
+		nextStepBoardColumns = transpose (autoSolution (transpose board) top tAuto)
+		finishBoard 				 = autoGen (emptyBoard i j) left top lAuto tAuto 0
 handler _ field = field
-
-updater _ Field { board       = board
-				, boardSize   = (i, j)
-				, leftPanel   = left
-				, topPanel    = top
-				, leftAutomat = lAuto
-				, topAutomat  = tAuto
-				, panelSize   = panelSize
-				, gameOver    = flag
-				}
-	= Field { board       = board
-			, boardSize   = (i, j)
-			, leftPanel   = left
-			, topPanel    = top
-			, leftAutomat = lAuto
-			, topAutomat  = tAuto
-			, panelSize   = panelSize
-			, gameOver    = checkToWin board lAuto
-			}
+--Обновитель мира, который каждый раз обновляет флаг конца игры GameOver
+updater :: Float -> Field -> Field
+updater _ f@Field { board       = board
+				  				, solverAuto  = Auto { leftAutomat = lAuto }
+				 					, gameOver    = flag 
+				 					}
+	= f { gameOver = checkToWin board lAuto }
